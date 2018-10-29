ed6bb51e8d072e6ca1ec8a862296c09731d59180
albertoChang <alberto.changl@pucp.pe>
2018-10-29 17:56:03 -0500

    enemies
    enemy attack
    enemy movement

---
+++ Assets/Characters/Scripts/EnemyAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+    public GameObject tower;
+
+    Animator anim;
+    //TowerHealth towerhealth;
+    //EnemyHealth enemyhealth;
+    bool towerInRange;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        //towerhealth = tower.GetComponent<TowerHealth>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == tower)
+        {
+            anim.SetTrigger("Collision");
+            towerInRange = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        timer += Time.deltaTime;
+
+        if (timer>= timeBetweenAttacks && towerInRange)
+        {
+            //Attack();
+        }
+
+	}
+}
---
+++ Assets/Characters/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject tower;
+
+    Animator anim;
+    Transform target;
+    NavMeshAgent nav;
+    Transform cachedTransform;
+
+	// Use this for initialization
+	void Start () {
+        cachedTransform = transform;
+        target = cachedTransform;
+
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+	}
+
+    public void activateAgent()
+    {
+        anim.SetTrigger("StartRunning");
+        target = tower.transform;
+        nav.SetDestination(target.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == tower)
+        {
+            nav.isStopped = true ;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
