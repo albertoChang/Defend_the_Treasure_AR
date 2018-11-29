cafe30afe7c2be6262f8f589a802a93564769719
albertoChang <alberto.changl@pucp.pe>
2018-11-28 23:48:07 -0500

    Audio and loading screens
    Audio in mainMenu, gamelvl, victory and death.
    loadingscreen between scenes.

---
+++ Assets/MyScripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject tower;
+    EnemyAttack enemy_att;
+
+    Animator anim;
+    Transform target;
+    NavMeshAgent nav;
+    Transform cachedTransform;
+
+	// Use this for initialization
+	void Start () {
+        cachedTransform = transform;
+        target = cachedTransform;
+
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+        enemy_att = GetComponent<EnemyAttack>();
+	}
+
+    public void activateAgent()
+    {
+        anim.SetTrigger("StartRunning");
+        target = tower.transform;
+        nav.SetDestination(target.position);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == tower.name)
+        {
+            nav.isStopped = true;
+            anim.SetTrigger("Collision");
+            enemy_att.FoundTower(true);
+        }
+    }
+    
+}
---
+++ Assets/MyScripts/GameManager/LevelLoader.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public GameObject textmeshPro;
+
+    TextMeshProUGUI progressText;
+
+    public void SceneLoader(int sceneIndex)
+    {
+        progressText = textmeshPro.GetComponent<TextMeshProUGUI>();
+
+        if (sceneIndex == 0)
+            FindObjectOfType<AudioManager>().Stop("Loop 1 Tension"); 
+        else if (sceneIndex == 1)
+            FindObjectOfType<AudioManager>().Stop("Blood and Steel Loop");
+
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+
+        //FindObjectOfType<AudioManager>().Stop("At the Gates");
+
+        if (sceneIndex == 0)
+            FindObjectOfType<AudioManager>().Play("Blood and Steel Loop");
+        else if (sceneIndex == 1)
+            FindObjectOfType<AudioManager>().Play("Loop 1 Tension");
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+        //FindObjectOfType<AudioManager>().Play("At the Gates");
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+
+        
+
+        
+    }
+}
